62a3992e1181337ab794025740a2ab1dc837cb29
ReDiana <harry886901@gmail.com>
2022-03-22 14:52:05 +0800

    最初版本

---
+++ src/main/java/com/example/rclab/setup/clientsetup.java
@@ -0,0 +1,11 @@
+package com.example.rclab.setup;
+
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class clientsetup {
+
+    public static void init(FMLClientSetupEvent event)
+    {
+
+    }
+}
